c8fd0abc74e3745a18681e61efea1e1369f7a094
Sunchhay Sin <sinsunchhay1995@gmail.com>
2026-09-02 16:11:11 +0700

    Added Profile Routes: Change Usernanme/Email/Password + Save/Fetch profile_image

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -14,6 +14,10 @@ router.post("/reg", async (req, res) => {
       return res.status(400).json({ error: "Missing Registration Fields" });
     }
 
+    if(password.length < 8) {
+      return res.status(400).json({ error: "Password must be at least 8 characters long" });
+    }
+
     const encryptPass = await bcrypt.hash(password, 10);
     const query = "INSERT INTO User (username, email, password) VALUES (?,?,?)";
     connection.query(query, [username, email, encryptPass], (err, result) => {
@@ -51,7 +55,7 @@ router.post("/login", async (req, res) => {
       const pass = await bcrypt.compare(password, user.password);
 
       if (!pass) {
-        return res.status(401).json({ error: "Error Login" });
+        return res.status(401).json({ error: "Incorrect Credentials" });
       }
 
       // create token with user data inside
@@ -85,25 +89,23 @@ router.get("/auth/me", (req, res) => {
 
   try {
     const decoded = jwt.verify(token, SECRET);
-    console.log(decoded);
-    res.json({
-      userID: decoded.userID,
-      username: decoded.username,
-      email: decoded.email,
+    const query = "SELECT userID, username, email FROM User WHERE userID = ?";
+    connection.query(query, [decoded.userID], (err, rows) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: "Database Error" });
+      }
+
+      if (rows.length === 0) {
+        return res.status(404).json({ error: "User not found" });
+      }
+
+      res.json(rows[0]); // { userID, username, email }
     });
   } catch (err) {
-    console.lerror(err.message);
+    console.error(err.message);
     return res.status(401).json({ error: "Invalid or expired token" });
   }
-});
-
-router.post("/logout", (req, res) => {
-  res.clearCookie("token", {
-    httpOnly: true,
-    secure: true,
-    sameSite: "none",
-  });
-  res.json({ success: true, message: "Logged out" });
 });
 
 export default router
